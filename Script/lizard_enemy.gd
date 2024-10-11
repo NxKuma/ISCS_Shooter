7ffc5bc0981622c34a74a72502599b00c1b9e06b
Mikh Villanueva <mikhael.villanueva@student.ateneo.edu>
2024-10-11 11:35:22 +0800

    edited lizard enemy
    deletes lizard enemy if too far out of screen

--- Script/lizard_enemy.gd
+++ Script/lizard_enemy.gd
@@ -2,15 +2,23 @@ extends Area2D
 
 @export var speed = 100.0
 @export var direction = Vector2.LEFT
-var shootspeed = 50
+var shoot_speed = 50
+var screen_size
 const PROJECTILE = preload("res://Scene/projectile.tscn")
+
+func _ready():
+	screen_size = get_viewport_rect().size 
 
 func _process(delta: float) -> void:
 	translate(direction * speed * delta)
-	global_position.y += sin(position.x * delta) * 6
+	global_position.y += sin(global_position.x * delta) * 6
 	
-	if Engine.get_process_frames() % shootspeed == 0:
+	if Engine.get_process_frames() % shoot_speed == 0:
 		shoot()
+	
+	#deletes the lizard instance when it is sufficiently out of screen
+	if global_position < (Vector2.ZERO + Vector2(-300,-300)) or global_position > (screen_size + Vector2(300,300)):
+		queue_free() 
 
 
 func shoot():
