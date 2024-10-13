9ea0c16e9f0b0c79d83b31327f87fec4d5dfe24d
EnnaColada <enna.veloso@student.ateneo.edu>
2024-10-13 19:38:09 +0800

    Player-Enemy Collision
    (Semi-working)

--- Script/lizard_enemy.gd
+++ Script/lizard_enemy.gd
@@ -26,3 +26,43 @@ func shoot():
 	new_projectile.direction = Vector2.LEFT
 	new_projectile.global_position = global_position
 	add_sibling(new_projectile)
+<<<<<<< Updated upstream
+=======
+
+
+func _on_area_entered(area):
+	if area.is_in_group("PlayerBullet") and !is_dead:
+		area.queue_free()
+		is_hit = true
+		global_position.x += 20
+		health -= 30
+		damaged()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group('Player'): 
+		body.health -= 10
+		health -= 50
+		damaged()
+		print_debug("snake")
+
+
+func damaged():
+	animated_sprite_2d.material.set("shader_parameter/Enabled", true)
+	wake.start()
+	if health <= 0:
+		is_dead = true
+		animated_sprite_2d.visible = false
+		collision_shape_2d.disabled = true
+		particle.emitting = true
+		set_process(false)
+		await get_tree().create_timer(1.0).timeout
+		queue_free()
+
+func _on_wake_timeout():
+	animated_sprite_2d.material.set("shader_parameter/Enabled", false)
+
+
+func _on_reload_timeout():
+	if !is_dead:
+		shoot()
+>>>>>>> Stashed changes
--- Script/snake_enemy.gd
+++ Script/snake_enemy.gd
@@ -9,7 +9,43 @@ func _ready():
 
 func _process(delta: float) -> void:
 	translate(direction * speed * delta)
+<<<<<<< Updated upstream
 
 	#deletes the lizard instance when it is sufficiently out of screen
 	if global_position < (Vector2.ZERO + Vector2(-300,-300)) or global_position > (screen_size + Vector2(300,300)):
 		queue_free() 
+=======
+	#deletes the snake instance when it is sufficiently out of screen
+	if global_position < (Vector2.ZERO + Vector2(-300,-300)) or global_position > (screen_size + Vector2(300,300)):
+		queue_free()
+
+func _on_area_entered(area):
+	if area.is_in_group("PlayerBullet") and !is_dead:
+		area.queue_free()
+		health -= 50
+		global_position.x += 20
+		damaged()
+		
+	
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group('Player'): 
+		body.health -= 30
+		health -= 50
+		damaged();
+		print_debug("hi snake")
+
+func damaged():
+	animated_sprite_2d.material.set("shader_parameter/Enabled", true)
+	wake.start()
+	if health <= 0:
+		is_dead = true
+		animated_sprite_2d.visible = false
+		snake.emitting = true
+		set_process(false)
+		await get_tree().create_timer(1.0).timeout
+		queue_free()
+
+
+func _on_wake_timeout():
+	animated_sprite_2d.material.set("shader_parameter/Enabled", false)
+>>>>>>> Stashed changes
